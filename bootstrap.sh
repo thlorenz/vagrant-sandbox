f0b26726915ef96771a46af7cea7d8369b5d9932
Thorsten Lorenz <thlorenz@gmx.de>
2014-03-21 11:13:44 -0400

    improving locale settings and switching powerline with airline to get nicer vim status line

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,8 @@ apt-get install -y nodejs npm git vim
 git clone https://github.com/thlorenz/dotfiles.git
 chown -R vagrant:vagrant dotfiles
 
+echo 'LANG=en_US.UTF-8' > /etc/default/locale
+
 rm -f .bashrc .profile
 
 # during provisioning ~ or $HOME seems to be /root so we need to fix that
@@ -13,7 +15,10 @@ export HOME=`pwd`
 
 ./dotfiles/scripts/create-links.sh
 
-(cd ./dotfiles && git submodule update --init)
+( cd ./dotfiles &&               \
+  git submodule update --init && \
+  rm -rf vim/bundle/powerline && \
+  git clone https://github.com/bling/vim-airline.git vim/bundle/airline)
 
 # start our node app
 # (cd /vagrant && npm start)
