166d51496f7298089bea31c803133bad1bfae49a
Thorsten Lorenz <thlorenz@gmx.de>
2014-04-04 13:57:34 -0400

    using 64-bit kernel and adding docker installation steps (including linux kernel upgrade)

--- bootstrap.sh
+++ bootstrap.sh
@@ -27,5 +27,18 @@ export HOME=`pwd`
   rm -rf vim/bundle/powerline && \
   git clone https://github.com/bling/vim-airline.git vim/bundle/airline)
 
+# docker
+
+## upgrade linux kernel to support aufs
+apt-get install -y linux-image-generic-lts-raring linux-headers-generic-lts-raring
+
+## not sure if we could install docker before upgrading kernel, so we'll reboot first and do it after
+echo 'apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9' > ./install-docker.sh
+echo 'sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"'   >> ./install-docker.sh 
+echo 'apt-get update'                                                                                   >> ./install-docker.sh
+echo 'apt-get install -y lxc-docker'                                                                    >> ./install-docker.sh
+
+echo "rebooting to activate new linux kernel please run 'sudo sh ./install-docker.sh' after logging in again"
+
 # start our node app
 # (cd /vagrant && npm start)
